62e9c669832b23c7386226bdfe2dfc433f787b87
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-24 18:49:28 +0100

    Added NixOS desktop template

--- flake.nix
+++ flake.nix
@@ -6,14 +6,20 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ ];
+      };
+    in {
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem { modules = [ ./hosts/desktop ]; };
+      };
+    };
 }
