a9fe02440b3eee3b5e375fffe97d113a93681f61
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-24 19:08:02 +0100

    Added bootloader config

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,5 +1,14 @@
 { config, lib, pkgs, ... }: 
 
 {
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
+    };
+  };
+
   system.stateVersion = "23.05";
 }
