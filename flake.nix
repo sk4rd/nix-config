f766e4b26697f9df5f175cccf7f9b8124a7fce11
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-24 18:28:31 +0100

    Added nixpkgs and home-manager as flake inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
 {
   description = "A very basic flake";
 
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+  };
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
