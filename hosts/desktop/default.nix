a41630ddc3e720114c05deec4a6850cd50f61e62
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-25 13:59:08 +0100

    Added basic plasma5 install

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -4,4 +4,9 @@
   imports = [ ../common ./hardware ];
 
   networking.hostName = "desktop";
+
+  # Basic Plasma5 install
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 }
