c73f39894fde5cdb70da8969a4a7d35122f724be
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-26 12:19:38 +0100

    Removed non-existant extension

--- home/miko/programs/github.nix
+++ home/miko/programs/github.nix
@@ -4,7 +4,7 @@
   # GitHub CLI configuration
   programs.gh = {
     enable = true;
-    extensions = with pkgs; [ gh-dash gh-markdown-preview gh-eco gh-s ];
+    extensions = with pkgs; [ gh-dash gh-markdown-preview gh-eco ];
     settings = {
       git_protocol = "ssh";
       prompt = "enabled";
