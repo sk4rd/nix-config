070d96498b30b25b4ccbd78c9a6a927a9a85975e
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-26 16:50:35 +0100

    Added amd cpu config to desktop

---
+++ hosts/desktop/hardware/cpu.nix
@@ -0,0 +1,5 @@
+{ ... }:
+
+{
+  hardware.cpu.amd.updateMicrocode = true;
+}
--- hosts/desktop/hardware/default.nix
+++ hosts/desktop/hardware/default.nix
@@ -1,5 +1,5 @@
 { ... }:
 
 {
-  imports = [ ./gpu.nix ./filesystem.nix ];
+  imports = [ ./cpu.nix ./gpu.nix ./filesystem.nix ];
 }
