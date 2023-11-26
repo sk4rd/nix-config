817231a8f912ec744e7602d7c1d4690eed0a6bb1
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-26 11:44:56 +0100

    Changed default git branch

--- home/miko/programs/git.nix
+++ home/miko/programs/git.nix
@@ -7,6 +7,7 @@
     package = pkgs.gitFull;
     userName  = "Mikolaj Bajtkiewicz";
     userEmail = "mikolaj.bajtkiewicz@tutanota.de";
+    config.init.defaultBranch = "main";
     aliases = {
       a = "add";
       br = "branch";
