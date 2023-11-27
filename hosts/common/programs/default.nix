373628169c9e3e1e50ff2f840ac72743b8a7d575
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-27 20:40:29 +0100

    Added virt-manager to system packages

--- hosts/common/programs/default.nix
+++ hosts/common/programs/default.nix
@@ -1,5 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./git.nix ./steam.nix ./zsh.nix ];
+
+  # Programs that don't require configuration
+  environment.systemPackages = with pkgs; [ virt-manager ];
 }
