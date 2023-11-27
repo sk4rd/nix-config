d5b767c28862424b2a70aeb39a15f11b44ccd696
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-27 17:33:47 +0100

    Added mangohud to programs

--- home/miko/programs/default.nix
+++ home/miko/programs/default.nix
@@ -17,6 +17,7 @@
     gimp # Advanced image editing tool
     keepassxc # Secure password management
     lutris # Game launcher
+    mangohud # Game performance menu
     monero-gui # Cryptocurrency wallet for Monero
     prismlauncher # Launcher for Minecraft
     protonup-qt # Proton version manager
