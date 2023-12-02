73648a08cf8cb5e2a6b550db45b678ab4240c1fc
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-02 19:51:06 +0100

    feat: added mime-type configuration
    - Added mime-type handling for images using imv
    - Added mime-type handling for videos using mpv
    - Added mime-type handling for various text files using emacs

--- home/miko/xdg.nix
+++ home/miko/xdg.nix
@@ -1,5 +1,47 @@
-{ ... }:
-let home = "/home/miko";
+{ pkgs, ... }:
+let
+  home = "/home/miko";
+  videoPlayer = "mpv.desktop";
+  imageViewer = "imv.desktop";
+  emacs = "emacs.desktop";
+  videoMimeTypes = [
+    "video/mp4"
+    "video/webm"
+    "video/x-matroska"
+    "video/x-msvideo"
+    "video/quicktime"
+    "video/x-ms-wmv"
+    "video/mpeg"
+    "video/ogg"
+    "video/x-flv"
+  ];
+  imageMimeTypes = [
+    "image/jpeg"
+    "image/png"
+    "image/gif"
+    "image/webp"
+    "image/tiff"
+    "image/svg+xml"
+  ];
+  programmingMimeTypes = [
+    "text/html"
+    "text/css"
+    "application/javascript"
+    "application/json"
+    "text/xml"
+    "application/x-python"
+    "application/x-shellscript"
+    "text/x-csrc"
+    "text/x-c++src"
+    "application/x-java"
+  ];
+  configMimeTypes = [ "text/x-yaml" "application/x-toml" ];
+  textMimeTypes = [ "text/plain" "text/markdown" "text/x-tex" ];
+  emacsSpecificMimeTypes = [
+    "text/x-nix"
+    "text/x-shellscript"
+    "text/plain" # for .conf, .org, .el files
+  ];
 in {
   xdg = {
     enable = true;
@@ -16,6 +58,21 @@ in {
       templates = "${home}/dsktp";
       videos = "${home}/vids";
       extraConfig = { XDG_SCREENSHOTS_DIR = "${home}/pics/screenshots"; };
+    };
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = builtins.listToAttrs (map (mimeType: {
+        name = mimeType;
+        value = [ videoPlayer ];
+      }) videoMimeTypes ++ map (mimeType: {
+        name = mimeType;
+        value = [ imageViewer ];
+      }) imageMimeTypes ++ map (mimeType: {
+        name = mimeType;
+        value = [ emacs ];
+      }) (textMimeTypes ++ programmingMimeTypes ++ configMimeTypes
+        ++ emacsSpecificMimeTypes));
     };
   };
 }
