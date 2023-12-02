bfa071f515460158344fb43a81f2ca03ce08c8fb
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-02 09:55:27 +0100

    Added general variables to hyprland config

--- home/miko/programs/hyprland.nix
+++ home/miko/programs/hyprland.nix
@@ -7,21 +7,33 @@ let
 in {
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.extraConfig = ''
-    # Screen settings
-    monitor = DP-1, 2560x1440@165, 1920x0, 1
-    monitor = HDMI-A-1, 1920x1080@60, 0x0, 1
+     # Screen settings
+     monitor = DP-1, 2560x1440@165, 1920x0, 1
+     monitor = HDMI-A-1, 1920x1080@60, 0x0, 1
 
-    # Keybindings
-    $mod = SUPER
-    bind = $mod, Q, exec, ${terminal}
-    bind = $mod, W, exec, ${browser}
-    bind = $mod, R, exec, ${launcher}
+     # Keybindings
+     $mod = SUPER
+     bind = $mod, Q, exec, ${terminal}
+     bind = $mod, W, exec, ${browser}
+     bind = $mod, R, exec, ${launcher}
 
-    # Input device settings
-    input {
-      kb_layout = us
-      follow_mouse = 1
-      accel_profile = flat
+     # Input device settings
+     input {
+       kb_layout = us
+       follow_mouse = 1
+       accel_profile = flat
+     }
+
+    # General Hyprland variables
+    general {
+      gaps_in = 5
+      gaps_out = 20
+      border_size = 3
+      col.active_border = rgba(fabd2fee) rgba(fe8019ee) 90deg
+      col.inactive_border = rgba(282828aa)
+      cursor_inactive_timeout = 5
+      layout = master
+      allow_tearing = false
     }
   '';
 }
