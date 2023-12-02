729b3f112451c5b383acd7b2f0fb86f147b1c43a
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-02 09:34:09 +0100

    Added very minimal hyprland configuration

--- home/miko/programs/hyprland.nix
+++ home/miko/programs/hyprland.nix
@@ -1,11 +1,28 @@
 { config, pkgs, ... }:
 
-{
+let
+  terminal = "${pkgs.kitty}/bin/kitty";
+  browser = "${pkgs.firefox}/bin/firefox";
+  launcher = "${pkgs.rofi-wayland}/bin/rofi -show run";
+in {
+  wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.extraConfig = ''
+    # Screen settings
+    monitor = DP-1, 2560x1440@165, 1920x0, 1
+    monitor = HDMI-A-1, 1920x1080@60, 0x0, 1
+
     # Keybindings
     $mod = SUPER
-    bind = $mod, W, exec, firefox
-    bind = $mod, R, exec, rofi -show run
+    bind = $mod, Q, exec, ${terminal}
+    bind = $mod, W, exec, ${browser}
+    bind = $mod, R, exec, ${launcher}
+
+    # Input device settings
+    input {
+      kb_layout = us
+      follow_mouse = 1
+      accel_profile = flat
+    }
   '';
 }
 
