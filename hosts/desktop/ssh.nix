004152988c345ff9b5222e37d4f392c8da2fefb6
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-12 21:56:15 +0100

    refactor(ssh): changed the pubkey

--- hosts/desktop/ssh.nix
+++ hosts/desktop/ssh.nix
@@ -3,7 +3,7 @@
 {
   # Allowed ssh keys for incoming connection
   users.users."miko".openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINru2QHxCuwsCc6yEtlLg4aoSI8KtuGg3SrZecZtKuCl miko@laptop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFYWLqJA2cfV/Wk8W3uQPZlTe3vdP/60584HghZIlHjT miko@laptop"
   ];
 }
 
