8a87c11880b5881217bc27e06ea3f39f26225619
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-12 18:20:53 +0100

    docs(flake): added more documentation

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Sk4rd's NixOS and Home-Manager Configuration";
 
+  # Definition of inputs (dependencies) for the flake
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
@@ -25,19 +26,24 @@
   outputs = { nixpkgs, home-manager, emacs, hyprland, split-monitor-workspaces
     , wallpapers, ... }@inputs:
     let
+      # Default system architecture
       system = "x86_64-linux";
+
+      # Package set from nixpkgs with specific system and configuration      
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
         overlays = [ ];
       };
-      # Function for creating a host configuration
+
+      # Function to create a NixOS configuration for a given host
       mkHostConfig = host:
         nixpkgs.lib.nixosSystem {
           inherit pkgs;
           modules = [ ./hosts/${host} ];
         };
-      # Function for creating a home configuration
+
+      # Function to create a Home Manager configuration for a given user and host
       mkHomeConfig = user: host:
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
@@ -48,10 +54,13 @@
           };
         };
     in {
+      # NixOS configurations for different hosts
       nixosConfigurations = {
         desktop = mkHostConfig "desktop";
         laptop = mkHostConfig "laptop";
       };
+
+      # Home Manager configurations for different users and hosts
       homeConfigurations = {
         "miko@desktop" = mkHomeConfig "miko" "desktop";
         "miko@laptop" = mkHomeConfig "miko" "laptop";
