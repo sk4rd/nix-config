f8f84454034dd8ba6b16fadc438dc896ff1e6d02
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-12 21:24:09 +0100

    feat(hyprland): added new desktop specific configuration

--- home/miko/desktop/hyprland.nix
+++ home/miko/desktop/hyprland.nix
@@ -1,18 +1,42 @@
 { ... }:
 
-{
-  wayland.windowManager.hyprland.extraConfig = ''
-    # Autostart settings
-    # Screen settings
-    monitor = DP-1, 2560x1440@165, 1920x0, 1
-    monitor = HDMI-A-1, 1920x1080@60, 0x0, 1
+let
+  # Primary and secondary monitor definitions
+  monitor = {
+    primary = {
+      identifier = "DP-1";
+      workspaces = [ 1 2 3 ];
+    };
+    secondary = {
+      identifier = "HDMI-A-1";
+      workspaces = [ 4 5 6 ];
+    };
+  };
 
-    # Input settings
-    input {
-      tablet {
-        output = DP-1
-      }
-    }
-  '';
+  # Function to create workspace bindings
+  mkWorkspaceBinding = monitorIdentifier: workspace:
+    "${toString workspace}, monitor:${monitorIdentifier}";
+
+  # Generate workspace bindings for a monitor
+  generateBindings = m:
+    builtins.map (ws: mkWorkspaceBinding m.identifier ws) m.workspaces;
+
+in {
+  # Additional Hyprland settings for desktop
+  wayland.windowManager.hyprland = {
+    settings = {
+      # Define monitor settings and layout
+      monitor = [
+        "${monitor.primary.identifier}, 2560x1440@165, 1920x0, 1"
+        "${monitor.secondary.identifier}, 1920x1080@60, 0x0, 1"
+      ];
+
+      # Bind workspaces to specific monitors
+      workspace = (generateBindings monitor.primary)
+        ++ (generateBindings monitor.secondary);
+
+      # Drawing tablet monitor binding
+      input = { tablet.output = monitor.primary.identifier; };
+    };
+  };
 }
-
