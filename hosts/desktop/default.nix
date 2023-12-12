91df377fd8e5ea2d89bc1f8d30a6f5c4e65e7955
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-12 21:50:38 +0100

    refactor: forgot to import ssh.nix module

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  imports = [ ../common ./hardware ./networking.nix ./boot.nix ];
+  imports = [ ../common ./hardware ./networking.nix ./boot.nix ./ssh.nix ];
 
   # Basic Plasma5 install
   services.xserver.enable = true;
