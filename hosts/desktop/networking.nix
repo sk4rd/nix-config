f4f398225ac7f3ec736bb4e716725a79db97ad53
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-13 13:14:17 +0100

    feat(networking): added static IP to desktop

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -1,5 +1,13 @@
 { ... }:
 
 {
-  networking = { hostName = "desktop"; };
+  networking = {
+    hostName = "desktop";
+    interfaces = {
+      enp10s0.ipv4.addresses = [{
+        address = "192.168.2.2";
+        prefixLength = 24;
+      }];
+    };
+  };
 }
