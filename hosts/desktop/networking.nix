844e8be3548fb6cc12ea93fd62dc47efc0de91ec
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-14 09:19:54 +0100

    Revert "feat(networking): added static IP to desktop"
    This reverts commit f4f398225ac7f3ec736bb4e716725a79db97ad53.

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -1,13 +1,5 @@
 { ... }:
 
 {
-  networking = {
-    hostName = "desktop";
-    interfaces = {
-      enp10s0.ipv4.addresses = [{
-        address = "192.168.2.2";
-        prefixLength = 24;
-      }];
-    };
-  };
+  networking = { hostName = "desktop"; };
 }
