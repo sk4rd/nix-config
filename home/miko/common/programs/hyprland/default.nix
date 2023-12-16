e2f30457da2d034aeeb907da2d492563cd7cbc1a
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-16 23:14:53 +0100

    refactor(hyprland): changed magic workspace binding to grave accent

--- home/miko/common/programs/hyprland/default.nix
+++ home/miko/common/programs/hyprland/default.nix
@@ -62,7 +62,7 @@ in {
         "$mod, 8, workspace, 8"
         "$mod, 9, workspace, 9"
         "$mod, 0, workspace, 10"
-        "$mod, S, togglespecialworkspace, magic"
+        "$mod, grave, togglespecialworkspace, magic"
 
         "$mod SHIFT, 1, movetoworkspace, 1"
         "$mod SHIFT, 2, movetoworkspace, 2"
@@ -74,7 +74,7 @@ in {
         "$mod SHIFT, 8, movetoworkspace, 8"
         "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod SHIFT, 0, movetoworkspace, 10"
-        "$mod SHIFT, S, movetoworkspace, special:magic"
+        "$mod SHIFT, grave, movetoworkspace, special:magic"
 
         # Window navigation/movement
         "$mod, h, movefocus, l"
