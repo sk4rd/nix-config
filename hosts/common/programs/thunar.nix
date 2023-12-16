d781c8e2efd73a6bbb07c005199c7b2734016b94
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-16 14:15:10 +0100

    feat(thunar): added thunar with plugins

---
+++ hosts/common/programs/thunar.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  # Thunar file manager configuration
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+      thunar-media-tags-plugin
+    ];
+  };
+
+  # Thumbnail support for images  
+  services.tumbler.enable = true;
+}
+
