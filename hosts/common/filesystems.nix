e4aa964c8688845cffa700249261831eb33be7bc
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-16 14:07:05 +0100

    feat(filesystems): enabled udisks2 and gvfs

--- hosts/common/filesystems.nix
+++ hosts/common/filesystems.nix
@@ -3,5 +3,11 @@
 {
   # Filesystem support configuration
   boot.supportedFilesystems = [ "ntfs" ];
+
+  # Enable udisks2
+  services.udisks2.enable = true;
+
+  # Enable gvfs
+  services.gvfs.enable = true;
 }
 
