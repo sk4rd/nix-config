f950bac6168938a23dca5854a2563a56bc8983fc
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-12-19 15:51:03 +0100

    refactor(flake): changed nixpkgs version from unstable to 23.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   # Definition of inputs (dependencies) for the flake
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
