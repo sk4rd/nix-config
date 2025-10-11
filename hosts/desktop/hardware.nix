9e1cedae597b311818566bc7ff0ec71216aea4a8
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-10-11 22:31:19 +0200

    feat(hosts/desktop): add opencl support

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware = {
@@ -7,6 +7,9 @@
     graphics = {
       enable = true;
       enable32Bit = true;
+      extraPackages = with pkgs; [
+        rocmPackages.clr.icd
+      ];
     };
   };
 }
