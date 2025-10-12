9ed49c08894e40aa1ea1927491516d4686f16105
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-10-12 12:21:31 +0200

    feat(hosts/desktop): use wayland with sddm

--- hosts/desktop/services.nix
+++ hosts/desktop/services.nix
@@ -3,6 +3,7 @@
 {
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
