14340a1324a61d000a452b178e8bdfc1d7f82730
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-10-12 16:59:22 +0200

    feat(home/miko): add basic js ide functionality to nvim

--- home/miko/programs.nix
+++ home/miko/programs.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.neovim = {
@@ -6,5 +6,39 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      coc-tsserver
+      coc-prettier
+    ];
+
+    coc = {
+      enable = true;
+      settings = {
+        "coc.preferences.formatOnSaveFiletypes" = [
+          "css"
+          "markdown"
+          "javascript"
+        ];
+      };
+      pluginConfig = ''
+        inoremap <silent><expr> <cr> coc#pum#visible() ? coc#_select_confirm() : "\<C-g>u\<CR>"
+
+        " use <tab> to trigger completion and navigate to the next complete item
+        function! CheckBackspace() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col - 1]  =~# '\s'
+        endfunction
+
+        inoremap <silent><expr> <Tab>
+          \ coc#pum#visible() ? coc#pum#next(1) :
+          \ CheckBackspace() ? "\<Tab>" :
+          \ coc#refresh()
+      '';
+    };
+
+    extraConfig = ''
+      set nu
+      set rnu
+    '';
   };
 }
