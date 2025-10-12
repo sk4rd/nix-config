35c6978acd82cc8d4c97440313f0a3c3063268bb
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-10-12 22:41:26 +0200

    feat(home/miko): add logseq to packages

--- home/miko/default.nix
+++ home/miko/default.nix
@@ -1,10 +1,11 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
 
   home-manager.users.miko = {
     imports = [ ./programs.nix ];
+    home.packages = with pkgs; [ logseq ];
     home.stateVersion = "25.05";
   };
 }
