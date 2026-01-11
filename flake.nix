a0681685747c2f2b8d0d03e97f15fa539a642711
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-01-11 11:30:00 +0100

    refactor(deskto): enable libvirt with vfio passthrough

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
