003b9697cdea4ec400ed39372f9ec227af6e40d3
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-01-17 11:49:40 +0100

    chore: formatted files

--- hosts/desktop/filesystem.nix
+++ hosts/desktop/filesystem.nix
@@ -1,18 +1,21 @@
 { ... }:
 
 {
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b6c024af-2e20-472a-bc60-ab93207abc8c";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/b6c024af-2e20-472a-bc60-ab93207abc8c";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3CD7-717D";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/d2defb9c-dec4-4dfb-860f-4ebd39220504"; }
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/3CD7-717D";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
     ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/d2defb9c-dec4-4dfb-860f-4ebd39220504"; }
+  ];
 }
