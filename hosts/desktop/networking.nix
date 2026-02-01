ac1ac65370b49ce09d18eff5f1a3618777c2daa5
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-02-01 13:01:33 +0100

    feat(networking): add bridge device while disabling networkmanager

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -3,6 +3,10 @@
 {
   networking = {
     hostName = "desktop";
-    networkmanager.enable = true;
+    useNetworkd = true;
+    useDHCP = false;
+    interfaces.enp10s0.useDHCP = true;
+    interfaces.br0.useDHCP = true;
+    bridges.br0.interfaces = [ "enp10s0" ];
   };
 }
