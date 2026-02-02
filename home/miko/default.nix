c79400d44e716fa8910eafc5c3faeced5457fb1e
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-02-02 11:20:37 +0100

    refactor(flake): separate homemanager from nixos modules

--- home/miko/default.nix
+++ home/miko/default.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
+
 {
-  home-manager.useGlobalPkgs = true;
-  home-manager.users.miko = {
-    imports = [ ./programs.nix ];
-    home.packages = with pkgs; [
-      logseq
-      orca-slicer
-      spotify
-    ];
-    home.stateVersion = "25.05";
-  };
+  imports = [ ./programs.nix ];
+
+  home.packages = with pkgs; [
+    logseq
+    orca-slicer
+    spotify
+  ];
+
+  home.stateVersion = "25.05";
 }
