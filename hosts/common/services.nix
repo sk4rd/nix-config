1e31b6a043a02233019086e7849504f24a38c0c9
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-02-07 16:22:52 +0100

    feat(common): enable OpenSSH with secure defaults

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -1,6 +1,14 @@
 { ... }:
 
 {
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
