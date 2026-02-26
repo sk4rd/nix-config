c1d53542925a307da2daaa8c4373bc9be84df0d0
Mikolaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-02-26 13:24:55 +0100

    refactor(emacs): add more corfu settings

--- home/common/emacs/init.el
+++ home/common/emacs/init.el
@@ -26,6 +26,7 @@
 (recentf-mode 1)
 (savehist-mode 1)
 (global-auto-revert-mode 1)
+(setq switch-to-buffer-obey-display-actions t)
 
 ;; Indentation
 (setq-default indent-tabs-mode nil)
@@ -87,13 +88,39 @@
 ;;; In-buffer completion
 
 (use-package corfu
-  :ensure nil
+  :ensure t
+  ;; Optional customizations
   :custom
-  (corfu-auto t)
-  (corfu-auto-delay 0.2)
-  (corfu-auto-prefix 2)
+  (corfu-cycle t)                 ; Allows cycling through candidates
+  (corfu-auto t)                  ; Enable auto completion
+  (corfu-auto-prefix 2)           ; Minimum length of prefix for completion
+  (corfu-auto-delay 0)            ; No delay for completion
+  (corfu-popupinfo-delay '(0.5 . 0.2))  ; Automatically update info popup after that numver of seconds
+  (corfu-preview-current 'insert) ; insert previewed candidate
+  (corfu-preselect 'prompt)
+  (corfu-on-exact-match nil)      ; Don't auto expand tempel snippets
+  ;; Optionally use TAB for cycling, default is `corfu-complete'.
+  :bind (:map corfu-map
+              ("M-SPC"      . corfu-insert-separator)
+              ("TAB"        . corfu-next)
+              ([tab]        . corfu-next)
+              ("S-TAB"      . corfu-previous)
+              ([backtab]    . corfu-previous)
+              ("S-<return>" . corfu-insert)
+              ("RET"        . corfu-insert))
+
   :init
-  (global-corfu-mode 1))
+  (global-corfu-mode)
+  (corfu-history-mode)
+  (corfu-popupinfo-mode) ; Popup completion info
+  :config
+  (add-hook 'eshell-mode-hook
+            (lambda () (setq-local corfu-quit-at-boundary t
+                                   corfu-quit-no-match t
+                                   corfu-auto nil)
+              (corfu-mode))
+            nil
+            t))
 
 (use-package cape
   :ensure nil
