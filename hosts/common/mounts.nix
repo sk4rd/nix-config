819bac5eea61d231c21d94d810012fa6c25219c0
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-03-01 17:04:11 +0100

    refactor(hosts): add torrent mount

--- hosts/common/mounts.nix
+++ hosts/common/mounts.nix
@@ -3,8 +3,8 @@
 {
   environment.systemPackages = [ pkgs.cifs-utils ];
 
-  fileSystems."/mnt/nas" = {
-    device = "//192.168.178.3/private";
+  fileSystems."/mnt/nas/documents" = {
+    device = "//192.168.178.3/Documents";
     fsType = "cifs";
     options = [
       "x-systemd.automount"
@@ -17,4 +17,20 @@
       "gid=100"
     ];
   };
+
+  fileSystems."/mnt/nas/torrents" = {
+    device = "//192.168.178.3/Torrents";
+    fsType = "cifs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "credentials=${config.sops.secrets."nas/credentials".path}"
+      "uid=1000"
+      "gid=100"
+    ];
+  };
+
 }
