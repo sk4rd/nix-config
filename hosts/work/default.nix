8fdfe70327b1935bcd3188bc603e1c8ae0fbac27
Mikolaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-03-06 10:14:55 +0100

    feat(work): add default proxy

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -7,8 +7,13 @@
     secrets."proxy_extra_hosts" = { };
   };
 
+  networking.proxy.default = "http://127.0.0.1:3128";
   networking.hostFiles = [ config.sops.secrets."proxy_extra_hosts".path ];
 
+  wsl = {
+    enable = true;
+    wslConf.network.generateHosts = false;
+  };
+
   system.stateVersion = "25.11";
-  wsl.enable = true;
 }
