163830ad5caea05aa57719d9db51e256988743b9
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-03-08 13:14:24 +0100

    feat(mango): add more modules to waybar

--- home/miko/mango/waybar.nix
+++ home/miko/mango/waybar.nix
@@ -9,34 +9,65 @@
     settings.main = {
       layer = "top";
       position = "top";
-      height = 26;
-      spacing = 8;
+      height = 30;
+      spacing = 0;
       modules-left = [ "ext/workspaces" ];
       modules-right = [
+        "cpu"
+        "memory"
+        "temperature"
         "pulseaudio"
         "network"
-        "battery"
         "clock"
-        "tray"
       ];
 
       "ext/workspaces" = {
-        "format" = "{icon}";
+        "format" = "{id}";
         "ignore-hidden" = true;
         "sort-by-id" = true;
         "all-outputs" = false;
         "on-click" = "activate";
         "on-click-right" = "deactivate";
+        "tooltip" = false;
+      };
+
+      cpu = {
+        "interval" = 2;
+        "format" = "CPU {usage}%";
+        "states" = {
+          "warning" = 70;
+          "critical" = 90;
+        };
+        "tooltip" = false;
+      };
+
+      memory = {
+        "interval" = 5;
+        "format" = "RAM {percentage}%";
+        "states" = {
+          "warning" = 75;
+          "critical" = 90;
+        };
+        "tooltip" = false;
+      };
+
+      temperature = {
+        "interval" = 5;
+        "format" = "TMP {temperatureC}°C";
+        "format-warning" = "TMP {temperatureC}°C";
+        "format-critical" = "TMP {temperatureC}°C";
+        "warning-threshold" = 75;
+        "critical-threshold" = 85;
+        "hwmon-path-abs" = [
+          "/sys/devices/pci0000:00/0000:00:18.3/hwmon"
+        ];
+        "input-filename" = "temp1_input";
+        "tooltip" = false;
       };
 
       pulseaudio = {
-        "format" = "{icon} {volume}%";
+        "format" = "VOL {volume}%";
         "format-muted" = "MUTED";
-        "format-icons" = [
-          "VOL"
-          "VOL"
-          "VOL"
-        ];
         "scroll-step" = 5;
         "tooltip" = false;
       };
@@ -48,24 +79,9 @@
         "tooltip-format" = "{ifname} {ipaddr}";
       };
 
-      battery = {
-        "format" = "BAT {capacity}%";
-        "format-charging" = "CHG {capacity}%";
-        "format-plugged" = "AC";
-        "states" = {
-          "warning" = 25;
-          "critical" = 12;
-        };
-      };
-
       clock = {
         "format" = "{:%a %Y-%m-%d %H:%M}";
         "tooltip" = false;
-      };
-
-      tray = {
-        "icon-size" = 14;
-        "spacing" = 6;
       };
     };
   };
