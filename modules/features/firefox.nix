8677c3d7d1624c8c206e6806830b589ed96336fa
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-08-31 00:23:07 +0200

    Add Proton Pass to Firefox

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -7,10 +7,16 @@
         DisableFirefoxStudies = true;
         DisablePocket = true;
         DisableTelemetry = true;
+        PasswordManagerEnabled = false;
 
         ExtensionSettings."uBlock0@raymondhill.net" = {
           installation_mode = "normal_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+
+        ExtensionSettings."78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
         };
       };
     };
