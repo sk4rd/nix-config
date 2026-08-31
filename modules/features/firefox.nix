c67e11e441ef5c852f48de054bda55a651d73863
Mikołaj Bajtkiewicz <mikolaj.ba@pm.me>
2026-08-31 23:08:15 +0200

    Remove managed desktop defaults

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -20,14 +20,5 @@
         };
       };
     };
-
-    xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = [ "firefox.desktop" ];
-        "x-scheme-handler/http" = [ "firefox.desktop" ];
-        "x-scheme-handler/https" = [ "firefox.desktop" ];
-      };
-    };
   };
 }
